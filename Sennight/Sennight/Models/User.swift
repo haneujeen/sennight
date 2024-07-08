523b95d3be5986c23c3b9b2558a111a53279a4ac
sykr1313 <167267895+sykr1313@users.noreply.github.com>
2024-07-08 15:42:32 +0900

    양파마켓 코드 참고하며 테스트 중

--- Sennight/Sennight/Models/User.swift
+++ Sennight/Sennight/Models/User.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct LoginRequest: Codable {
+    let userName : String
+    let password : String
+}
+
+struct Member : Codable {
+    let userName : String
+    let profile : String
+}
+
+struct LoginResponse : Codable {
+    let success : Bool
+    let token : String
+    let member : Member
+    let message : String
+}
+
+struct RegisterResponse : Codable {
+    let success : Bool
+    let message : String
+}
--- Sennight/Sennight/ViewModels/UserViewModel.swift
+++ Sennight/Sennight/ViewModels/UserViewModel.swift
@@ -7,8 +7,13 @@
 
 import Foundation
 import SwiftUI
+import Combine
 
 class UserViewModel: ObservableObject {
-    @Published var isLoggedIn: Bool = true
+    @Published var isLoggedIn: Bool = false
+    @Published var password : String = "1234"
+    @Published var userName : String = "sample@naver.com"
+    
+    private var cancellable = Set<AnyCancellable>
 }
 
