64fb4071f5c76e7e9db104d35a5b5982da5ce5da
sykr1313 <167267895+sykr1313@users.noreply.github.com>
2024-07-11 09:38:45 +0900

    UserService 구현

--- Sennight/Sennight/Services/UserService.swift
+++ Sennight/Sennight/Services/UserService.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+import Combine
+import Alamofire
+
+class UserService {
+    static let shared = UserService() //싱글톤 패턴 공유 인스턴스 생성
+    let tokenkey = "token"
+    let host = "http://127.0.0.1"
+    let port = "8080"
+    //토큰을 UserDefaults에 저장
+    func saveToken(_ token: String) {
+        UserDefaults.standard.set(token, forKey: tokenkey)
+    }
+    //UserDefaults에서 토큰을 가져오는 메소드
+    func getToken() -> String? {
+        UserDefaults.standard.string(forKey: tokenkey)
+    }
+    //로그인 상태인지 확인
+    func isLoggedIn() -> Bool {
+        getToken() != nil
+    }
+    //로그아웃, 토큰을 UserDefaults에서 삭제
+    func logout() {
+        UserDefaults.standard.removeObject(forKey: tokenkey)
+    }
+    
+    //회원가입 Publisher 생성
+    func register(email: String, password: String, name: String)->AnyPublisher<Register, AFError> {
+        let url = "\(host):\(port)/mock/users/register"
+        return AF.upload(multipartFormData: { MultipartFormData in
+            MultipartFormData.append(email.data(using: .utf8)!, withName: "email")
+            MultipartFormData.append(password.data(using: .utf8)!, withName: "password")
+            MultipartFormData.append(name.data(using: .utf8)!, withName: "name")
+        }, to: url)
+        .publishDecodable(type: Register.self) //응답을 Register 타입으로 디코딩
+        .value() //디코딩된 값을 반환
+        .eraseToAnyPublisher()
+    }
+    
+    //로그인 Publisher 생성
+    func login(email: String, password: String)->AnyPublisher<LoginResponse, AFError> {
+        let url = "\(host):\(port)/mock/users"
+        let body = LoginRequest(email: email, password: password)
+        
+        return AF.request(url, method: .post, parameters: body, encoder: JSONParameterEncoder.default)
+            .publishDecodable(type: LoginResponse.self) //응답을 LoginResponse 타입으로 디코딩
+            .value() //디코딩된 값을 반환
+            .eraseToAnyPublisher()
+    }
+}
