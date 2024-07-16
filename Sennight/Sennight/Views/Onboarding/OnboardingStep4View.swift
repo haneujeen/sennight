c8ea72652606402cbad419573db1419242227dc2
Eujeen Han <eujeenhan@gmail.com>
2024-07-16 17:53:54 +0900

    SEN-24 Add new onboarding steps

---
+++ Sennight/Sennight/Views/Onboarding/OnboardingStep4View.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingStep4View.swift
+//  Sennight
+//
+//  Created by 한유진 on 7/16/24.
+//
+
+import SwiftUI
+
+struct OnboardingStep4View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingStep4View()
+}
---
+++ Sennight/Sennight/Views/Onboarding/OnboardingStep5View.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingStep5View.swift
+//  Sennight
+//
+//  Created by 한유진 on 7/16/24.
+//
+
+import SwiftUI
+
+struct OnboardingStep5View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingStep5View()
+}
