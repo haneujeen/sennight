4b4073251c3a54a4bd5d3e0e89a42f71aedd52ab
sykr1313 <167267895+sykr1313@users.noreply.github.com>
2024-07-16 17:45:49 +0900

    UserSerivice의 회원가입 Publisher 생성 코드 수정

--- Sennight/Sennight/Services/UserService.swift
+++ Sennight/Sennight/Services/UserService.swift
@@ -38,15 +38,48 @@ class UserService {
     // 회원가입 Publisher 생성
     func register(email: String, password: String, name: String) -> AnyPublisher<RegisterResponse, AFError> {
         let url = "\(HOST)/users/register"
-        return AF.upload(multipartFormData: { MultipartFormData in
-            MultipartFormData.append(email.data(using: .utf8)!, withName: "email")
-            MultipartFormData.append(password.data(using: .utf8)!, withName: "password")
-            MultipartFormData.append(name.data(using: .utf8)!, withName: "name")
-        }, to: url)
-        .publishDecodable(type: RegisterResponse.self) // 응답을 RegisterResponse 타입으로 디코딩
-        .value() // 디코딩된 값을 반환
+        let body = RegisterRequest(email: email, password: password, name: name)
+        return AF.request(url,
+                          method: .post,
+                          parameters: body,
+                          encoder: JSONParameterEncoder.default)
+        .publishData()
+        .tryMap { result -> Data in
+            if let data = result.data {
+                // 응답 데이터를 로그로 출력
+                if let jsonString = String(data: data, encoding: .utf8) {
+                    print("Raw JSON response: \(jsonString)")
+                } else {
+                    print("Failed to convert data to string")
+                }
+                return data
+            } else {
+                throw AFError.responseValidationFailed(reason: .dataFileNil)
+            }
+        }
+        .decode(type: RegisterResponse.self, decoder: JSONDecoder())
+        .mapError { error in
+            if let afError = error as? AFError {
+                return afError
+            } else {
+                return AFError.responseSerializationFailed(reason: .decodingFailed(error: error))
+            }
+        }
         .eraseToAnyPublisher()
     }
+    
+    //회원가입 Pubisher MultpartFormData방법으로 생성
+    //    func register(email: String, password: String, name: String) -> AnyPublisher<RegisterResponse, AFError> {
+    //        let url = "\(HOST)/users/register"
+    //        return AF.upload(multipartFormData: { MultipartFormData in
+    //            MultipartFormData.append(email.data(using: .utf8)!, withName: "email")
+    //            MultipartFormData.append(password.data(using: .utf8)!, withName: "password")
+    //            MultipartFormData.append(name.data(using: .utf8)!, withName: "name")
+    //        }, to: url)
+    //        .publishDecodable(type: RegisterResponse.self) // 응답을 RegisterResponse 타입으로 디코딩
+    //        .value() // 디코딩된 값을 반환
+    //        .eraseToAnyPublisher()
+    //    }
     
     // 로그인 Publisher 생성
     func login(email: String, password: String) -> AnyPublisher<LoginResponse, AFError> {
