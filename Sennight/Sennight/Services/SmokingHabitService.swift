fb27c3c683cf2bf7533f051bc071f4690838a20e
sykr1313 <167267895+sykr1313@users.noreply.github.com>
2024-07-17 15:32:29 +0900

    SmokingHabitService 코드 생성

--- Sennight/Sennight/Services/SmokingHabitService.swift
+++ Sennight/Sennight/Services/SmokingHabitService.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+import Combine
+import Alamofire
+
+class SmokingHabitService {
+    static let shared = SmokingHabitService()
+    
+    let HOST = Settings.shared.HOST
+    
+    //smoking habits create Publisher 생성
+    func createSH(userId: Int, dailyCigarettes: Int, cigarettePrice: Int, firstCigarette: String, smokingYears: Int) -> AnyPublisher<SmokingHabitResponse, AFError> {
+        let url = "\(HOST)/smoking-habits"
+        let body = SmokingHabitsRequest(userId: userId,
+                                        dailyCigarettes: dailyCigarettes,
+                                        cigarettePrice: cigarettePrice,
+                                        firstCigarette: firstCigarette,
+                                        smokingYears: smokingYears)
+        return AF.request(url,
+                          method: .post,
+                          parameters: body,
+                          encoder: JSONParameterEncoder.default)
+        .publishDecodable(type: SmokingHabitResponse.self)
+        .value()
+        .eraseToAnyPublisher()
+    }
+    //smoking habits read 생성
+    func readSH(userId: Int, dailyCigarettes: Int, cigarettePrice: Int, firstCigarette: String, smokingYears: Int) -> AnyPublisher<SmokingHabitResponse, AFError> {
+        let url = "\(HOST)/smoking-habits/:user_id"
+        let body = SmokingHabitsRequest(userId: userId,
+                                        dailyCigarettes: dailyCigarettes,
+                                        cigarettePrice: cigarettePrice,
+                                        firstCigarette: firstCigarette,
+                                        smokingYears: smokingYears)
+        return AF.request(url,
+                          method: .get,
+                          parameters: body,
+                          encoder: JSONParameterEncoder.default)
+        .publishDecodable(type: SmokingHabitResponse.self)
+        .value()
+        .eraseToAnyPublisher()
+    }
+    //smoking habits update 생성
+    func updateSH(habit_id: Int, userId: Int, dailyCigarettes: Int, cigarettePrice: Int, firstCigarette: String, smokingYears: Int) -> AnyPublisher<SmokingHabitResponse, AFError> {
+        let url = "\(HOST)/smoking-habits/:habit_id"
+        let body = SmokingHabitsRequest(userId: userId,
+                                        dailyCigarettes: dailyCigarettes,
+                                        cigarettePrice: cigarettePrice,
+                                        firstCigarette: firstCigarette,
+                                        smokingYears: smokingYears)
+        return AF.request(url,
+                          method: .put,
+                          parameters: body,
+                          encoder: JSONParameterEncoder.default)
+        .publishDecodable(type: SmokingHabitResponse.self)
+        .value()
+        .eraseToAnyPublisher()
+    }
+}
