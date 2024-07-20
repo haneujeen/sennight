ba444429d41294da2012f21fcd6dc0425f3d6729
Eujeen Han <113181428+haneujeen@users.noreply.github.com>
2024-07-20 09:39:35 +0900

    Fix not being navigated error

--- Sennight/Sennight/Views/CheckOnboardingView.swift
+++ Sennight/Sennight/Views/CheckOnboardingView.swift
@@ -4,13 +4,14 @@
 //
 //  Created by 한유진 on 7/2/24.
 //  Edited by 한유진 on 2024-07-19: EntryView.swift 파일 이름 변경
+//  Edited by 한유진 on 2024-07-19: Fix not being navigated error
 //
 
 import SwiftUI
 
 struct CheckOnboardingView: View {
     @AppStorage("isOnboardingComplete") private var isOnboardingComplete: Bool = false
-    var loginViewModel = LoginViewModel()
+    @StateObject var loginViewModel = LoginViewModel()
     
     var body: some View {
         if isOnboardingComplete {
