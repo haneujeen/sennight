8f07a5f5973b1b54b01b467da81808b65f7b19a6
Eujeen Han <113181428+haneujeen@users.noreply.github.com>
2024-07-20 21:43:44 +0900

    Update LottieView.swift

--- Sennight/Sennight/Views/HelpersAndCustomViews/LottieView.swift
+++ Sennight/Sennight/Views/HelpersAndCustomViews/LottieView.swift
@@ -12,20 +12,42 @@ struct LottieView: UIViewRepresentable {
     let name: String
     let loopMode: LottieLoopMode
     let animationSpeed: CGFloat
+    let contentMode: UIView.ContentMode
+    @Binding var play: Bool
     
-    init(name: String, loopMode: LottieLoopMode = .autoReverse, animationSpeed: CGFloat = 1) {
+    let animationView: LottieAnimationView
+    
+    init(name: String, 
+         loopMode: LottieLoopMode = .playOnce,
+         animationSpeed: CGFloat = 1,
+         contentMode: UIView.ContentMode = .scaleAspectFit,
+         play: Binding<Bool> = .constant(true)
+    ) {
         self.name = name
+        self.animationView = LottieAnimationView(name: name)
         self.loopMode = loopMode
         self.animationSpeed = animationSpeed
+        self.contentMode = contentMode
+        self._play = play
     }
     
-    func makeUIView(context: Context) -> LottieAnimationView {
-        let animationView = LottieAnimationView(name: name)
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: .zero)
+        view.addSubview(animationView)
+        animationView.contentMode = contentMode
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        animationView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         animationView.loopMode = loopMode
         animationView.animationSpeed = animationSpeed
-        animationView.play()
-        return animationView
+        return view
     }
     
-    func updateUIView(_ uiView: LottieAnimationView, context: Context) {     }
+    func updateUIView(_ uiView: UIView, context: Context) {
+        if play {
+            animationView.play { _ in
+                play = false
+            }
+        }
+    }
 }
