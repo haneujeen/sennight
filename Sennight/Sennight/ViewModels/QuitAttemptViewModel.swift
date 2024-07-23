b79ce2d3a3ae5703141d5b66607cc7680c52c954
Eujeen Han <113181428+haneujeen@users.noreply.github.com>
2024-07-24 05:39:37 +0900

    QuitAttemptViewModel stubs

--- Sennight/Sennight/ViewModels/QuitAttemptViewModel.swift
+++ Sennight/Sennight/ViewModels/QuitAttemptViewModel.swift
@@ -11,104 +11,71 @@ import Combine
 import SwiftUI
 
 class QuitAttemptViewModel: ObservableObject{
-    
-    @Published var userId = 0
-    @Published var startDate = ""
-    @Published var attemptId = 0
-    @Published var endDate = ""
-    @Published var isActive = false
+    @Published var quitAttempts: [QuitAttempt] = []
+    @Published var latestQuitAttempt: QuitAttempt?
+    @Published var milestones: [Milestone] = []
     
     private var cancellables = Set<AnyCancellable>()
     
-    //금연 시간 등록
-    func creat(completion: @escaping (Bool)->Void) {
-        QuitAttemptService.shared.createQA(startDate: startDate)
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                switch completion {
-                case .finished: break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { response in
-                print("서버응답: \(response)")
-                completion(response.status)
-            }.store(in: &cancellables)
+    /// Creates a new quitting smoking attempt.
+    func createQuitAttempt() {
+        
     }
-    //금연 시간 확인
-    func read(completion: @escaping (Bool)->Void) {
-        QuitAttemptService.shared.readQA(userId: userId)
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                switch completion {
-                case .finished: break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { response in
-                print("서버응답: \(response)")
-                completion(response.status)
-            }.store(in: &cancellables)
-    }
-    //금연 시도 모두 확인
-    func readAll(completion: @escaping (Bool)->Void) {
-        QuitAttemptService.shared.readAllQA(userId: userId)
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                switch completion {
-                case .finished: break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { response in
-                print("서버응답: \(response)")
-                completion(response.status)
-            }.store(in: &cancellables)
-    }
-    //금연 시간 변경
-    func Update(completion: @escaping (Bool)->Void) {
-        QuitAttemptService.shared.updateQA(attemptId: attemptId, startDate: startDate, endDate: endDate, isActive: isActive)
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                switch completion {
-                case .finished: break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { response in
-                print("서버응답: \(response)")
-                completion(response.status)
-            }.store(in: &cancellables)
-    }
-    //금연 시간 삭제
-    func delete(completion: @escaping (Bool)->Void) {
-        QuitAttemptService.shared.deleteQA(attemptId: attemptId, startDate: startDate, endDate: endDate, isActive: isActive)
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                switch completion {
-                case .finished: break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { response in
-                print("서버응답: \(response)")
-                completion(response.status)
-            }.store(in: &cancellables)
-    }
-    //금연 시간에 따른 메달 제공
-    func ReadMilestiones(completion: @escaping (Bool)->Void) {
-        QuitAttemptService.shared.ReadMilestionesQA(attemptId: attemptId, startDate: startDate, endDate: endDate, isActive: isActive)
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                switch completion {
-                case .finished: break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { response in
-                print("서버응답: \(response)")
-                completion(response.status)
-            }.store(in: &cancellables)
+    
+    /**
+     func fetchSales() {
+         SaleService.shared.fetchSales()
+             .sink { completion in
+                 switch completion {
+                 case .finished:
+                     break
+                 case .failure(let error):
+                     print(error.localizedDescription)
+                 }
+             } receiveValue: { documents in
+                 self.sales = documents
+             }
+             .store(in: &cancellables) // Retain counter += 1
 
+     }
+     
+     */
+    /// Retrieves the most recent quitting smoking attempt for the user and updates the `latestQuitAttempt` property.
+    func getLatestQuitAttempt() {
+        guard let userID = UserService.shared.getUserID() else {
+            return
+        }
+        QuitAttemptService.shared.getLatestQuitAttempt(userID: userID)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { quitAttempt in
+                self.latestQuitAttempt = quitAttempt
+            }
+            .store(in: &cancellables)
+    }
+    
+    /// Retrieves all quitting smoking attempts for the user and updates the `quitAttempts` property.
+    func getAllQuitAttempts() {
+        
+    }
+    
+    /// Deletes a quitting smoking attempt.
+    func deleteQuitAttempt() {
+        
+    }
+    
+    /// Updates an existing quitting smoking attempt.
+    func updateQuitAttempt() {
+        
+    }
+    
+    /// Retrieves milestones associated with a specific quitting smoking attempt and updates the `milestones` property.
+    func getMilestonesForQuitAttempt() {
+        
     }
 }
