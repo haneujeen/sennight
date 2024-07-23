851be775967cb28856f0b320b6f7cd1427b49e4c
Eujeen Han <113181428+haneujeen@users.noreply.github.com>
2024-07-24 02:19:55 +0900

    Milestone, MilestoneRequest,  MilestoneResponse 모델 추가

--- Sennight/Sennight/Models/Milestone.swift
+++ Sennight/Sennight/Models/Milestone.swift
@@ -3,6 +3,64 @@
 //  Sennight
 //
 //  Created by 한유진 on 6/27/24.
+//  Edited by 한유진 on 2024-07-24: Milestone, MilestoneRequest,  MilestoneResponse 모델 추가
 //
 
-import Foundation
+/**
+ - 요청:
+     {
+         "user_id": 1,
+         "milestone_id": 1,
+         "quit_attempt_id": 2
+     }
+ 
+ - 응답:
+     {
+         "status": true,
+         "detail": "User's milestone created successfully",
+         "data": {
+             "id": 8,
+             "user_id": 1,
+             "milestone_id": 1,
+             "quit_attempt_id": 2,
+             "date_achieved": "2024-07-24T01:50:27"
+         }
+     }
+ */
+
+import SwiftUI
+
+struct MilestoneRequest: Codable {
+    let userID: Int
+    let milestoneID: Int
+    let quitAttemptID: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case userID = "user_id"
+        case milestoneID = "milestone_id"
+        case quitAttemptID = "quit_attempt_id"
+    }
+}
+
+struct Milestone: Codable {
+    let id: Int
+    let userID: Int
+    let milestoneID: Int
+    let quitAttemptID: Int
+    let dateAchieved: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case userID = "user_id"
+        case milestoneID = "milestone_id"
+        case quitAttemptID = "quit_attempt_id"
+        case dateAchieved = "date_achieved"
+    }
+}
+
+struct MilestoneResponse: Codable {
+    let status: Bool
+    let detail: String
+    let data: Milestone?
+}
+
