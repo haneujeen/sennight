00f610ce90877a04c9597c730625812a07d729f1
sykr1313 <167267895+sykr1313@users.noreply.github.com>
2024-07-24 21:06:38 +0900

    Update MotivationService.swift
    onboardingToken 추가
    디버깅 문 추가

--- Sennight/Sennight/Services/MotivationService.swift
+++ Sennight/Sennight/Services/MotivationService.swift
@@ -16,20 +16,49 @@ class MotivationService {
     
     let HOST = Settings.shared.HOST
     
-    func createMotivation(userID: Int, motivationID: Int) -> AnyPublisher<MotivationResponse, AFError> {
+    func createMotivation(onboardingToken: String, userID: Int, motivationID: Int) -> AnyPublisher<MotivationResponse, AFError> {
         let url = "\(HOST)/user-motivations"
-        guard let token = UserService.shared.getToken() else {
-                    return Fail(error: AFError.createURLRequestFailed(error: URLError(.userAuthenticationRequired))).eraseToAnyPublisher()
-                }
+//        guard let token = UserService.shared.getToken() else {
+//                    return Fail(error: AFError.createURLRequestFailed(error: URLError(.userAuthenticationRequired))).eraseToAnyPublisher()
+//                }
         let body = MotivationRequest(userID: userID, motivationID: motivationID, userMotivationID: nil)
+        
+        do {
+            let jsonData = try JSONEncoder().encode(body)
+            if let jsonString = String(data: jsonData, encoding: .utf8) {
+                print("Request: \(jsonString)")
+            }
+        } catch {
+            print("Failed to encode: \(error)")
+        }
         return AF.request(url,
                           method: .post,
                           parameters: body,
                           encoder: JSONParameterEncoder.default,
-                          headers: ["Authorization": "Bearer \(token)"])
-            .publishDecodable(type: MotivationResponse.self)
-            .value()
-            .eraseToAnyPublisher()
+                          headers: ["Authorization": "Bearer \(onboardingToken)"])
+        .publishData()
+                .tryMap { result -> Data in
+                    if let data = result.data {
+                        // 응답 데이터를 로그로 출력
+                        if let jsonString = String(data: data, encoding: .utf8) {
+                            print("Raw JSON response: \(jsonString)")
+                        } else {
+                            print("Failed to convert data to string")
+                        }
+                        return data
+                    } else {
+                        throw AFError.responseValidationFailed(reason: .dataFileNil)
+                    }
+                }
+                .decode(type: MotivationResponse.self, decoder: JSONDecoder())
+                .mapError { error in
+                    if let afError = error as? AFError {
+                        return afError
+                    } else {
+                        return AFError.responseSerializationFailed(reason: .decodingFailed(error: error))
+                    }
+                }
+                .eraseToAnyPublisher()
     }
     
     func readMotivation(userID: Int) -> AnyPublisher<MotivationResponse, AFError> {
