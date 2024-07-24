2b1bfb80a27650a5f84a97d3aef9c1e390f22925
sykr1313 <167267895+sykr1313@users.noreply.github.com>
2024-07-24 21:12:33 +0900

    Update QuitAttemptViewModel.swift
    onboardingToken 추가
    함수명 오타 수정
    read, update, delete readMilestiones 삭제

--- Sennight/Sennight/ViewModels/QuitAttemptViewModel.swift
+++ Sennight/Sennight/ViewModels/QuitAttemptViewModel.swift
@@ -17,12 +17,13 @@ class QuitAttemptViewModel: ObservableObject{
     @Published var attemptID = 0
     @Published var endDate = ""
     @Published var isActive = false
+    @Published var onboardingToken = ""
     
     private var cancellables = Set<AnyCancellable>()
     
     //금연 시간 등록
-    func creat(completion: @escaping (Bool)->Void) {
-        QuitAttemptService.shared.createQuitAttempt(startDate: startDate, userID: userID)
+    func create(completion: @escaping (Bool)->Void) {
+        QuitAttemptService.shared.createQuitAttempt(onboardingToken: onboardingToken, startDate: startDate, userID: userID)
             .receive(on: DispatchQueue.main)
             .sink { completion in
                 switch completion {
@@ -34,70 +35,5 @@ class QuitAttemptViewModel: ObservableObject{
                 print("서버응답: \(response)")
                 completion(response.status)
             }.store(in: &cancellables)
-    }
-    //금연 시간 확인
-    func read(completion: @escaping (Bool)->Void) {
-        QuitAttemptService.shared.readQuitAttempt(userID: userID)
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                switch completion {
-                case .finished: break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { response in
-                print("서버응답: \(response)")
-                completion(response.status)
-            }.store(in: &cancellables)
-    }
-    
-    //금연 시도 모두 확인
-    // TODO: readAll() 추가
-    
-    //금연 시간 변경
-    func Update(completion: @escaping (Bool)->Void) {
-        QuitAttemptService.shared.updateQuitAttempt(attemptID: attemptID, startDate: startDate, endDate: endDate, isActive: isActive)
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                switch completion {
-                case .finished: break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { response in
-                print("서버응답: \(response)")
-                completion(response.status)
-            }.store(in: &cancellables)
-    }
-    //금연 시간 삭제
-    func delete(completion: @escaping (Bool)->Void) {
-        QuitAttemptService.shared.deleteQuitAttempt(attemptID: attemptID, startDate: startDate, endDate: endDate, isActive: isActive)
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                switch completion {
-                case .finished: break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { response in
-                print("서버응답: \(response)")
-                completion(response.status)
-            }.store(in: &cancellables)
-    }
-    //금연 시간에 따른 메달 제공
-    func ReadMilestiones(completion: @escaping (Bool)->Void) {
-        QuitAttemptService.shared.ReadMilestionesQuitAttempt(attemptID: attemptID, startDate: startDate, endDate: endDate, isActive: isActive)
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                switch completion {
-                case .finished: break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { response in
-                print("서버응답: \(response)")
-                completion(response.status)
-            }.store(in: &cancellables)
-
     }
 }
