164191214f4e880f515a42daa0d00dd1e7e08cc5
sykr1313 <167267895+sykr1313@users.noreply.github.com>
2024-07-24 21:10:50 +0900

    Update MotivationViewModel.swift
    onboardingToken 추가
    read, update 삭제

--- Sennight/Sennight/ViewModels/MotivationViewModel.swift
+++ Sennight/Sennight/ViewModels/MotivationViewModel.swift
@@ -15,57 +15,13 @@ class MotivationViewModel: ObservableObject {
     @Published var userID = 0
     @Published var motivationID = 0
     @Published var userMotivationID = 0
+    @Published var onboardingToken = ""
     
     private var cancellables = Set<AnyCancellable>()
     
     //금연 동기 등록
-    func creat(completion: @escaping (Bool)->Void) {
-        MotivationService.shared.createMotivation(userID: userID, motivationID: motivationID)
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                switch completion {
-                case .finished: break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { response in
-                print("서버응답: \(response)")
-                completion(response.status)
-            }.store(in: &cancellables)
-    }
-    //금연 동기 등록
-    func read(completion: @escaping (Bool)->Void) {
-        MotivationService.shared.readMotivation(userID: userID)
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                switch completion {
-                case .finished: break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { response in
-                print("서버응답: \(response)")
-                completion(response.status)
-            }.store(in: &cancellables)
-    }
-    //금연 동기 수정
-    func update(completion: @escaping (Bool)->Void) {
-        MotivationService.shared.updateMotivation(userID: userID, motivationID: motivationID, userMotivationID: userMotivationID)
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                switch completion {
-                case .finished: break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { response in
-                print("서버응답: \(response)")
-                completion(response.status)
-            }.store(in: &cancellables)
-    }
-    //금연 동기 삭제
-    func delete(completion: @escaping (Bool)->Void) {
-        MotivationService.shared.deleteMotivation(userMotivationID: userMotivationID)
+    func create(completion: @escaping (Bool)->Void) {
+        MotivationService.shared.createMotivation(onboardingToken: onboardingToken, userID: userID, motivationID: motivationID)
             .receive(on: DispatchQueue.main)
             .sink { completion in
                 switch completion {
