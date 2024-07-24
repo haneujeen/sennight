f86d1734c21192ca7147c0655d12fe6f171d75ed
sykr1313 <167267895+sykr1313@users.noreply.github.com>
2024-07-24 21:14:03 +0900

    Update SmokingHabitViewModel.swift
    onboardingToken 추가
    cigarettePrice 타입변경
    update 삭제

--- Sennight/Sennight/ViewModels/SmokingHabitViewModel.swift
+++ Sennight/Sennight/ViewModels/SmokingHabitViewModel.swift
@@ -10,10 +10,11 @@ import Combine
 import SwiftUI
 
 class SmokingHabitViewModel: ObservableObject {
+    @Published var onboardingToken = ""
     @Published var userId = 0
     @Published var habitId = 0
     @Published var dailyCigarettes = 0
-    @Published var cigarettePrice = 0
+    @Published var cigarettePrice = 0.0
     @Published var firstCigarette = ""
     @Published var smokingYears = 0
     @Published var errorMessage: String? = nil
@@ -23,41 +24,7 @@ class SmokingHabitViewModel: ObservableObject {
     
     //흡연 정보 등록
     func create(completionHandler: @escaping (Bool)->Void) {
-        SmokingHabitService.shared.createSmokingHabit(userId: userId, dailyCigarettes: dailyCigarettes, cigarettePrice: cigarettePrice, firstCigarette: firstCigarette, smokingYears: smokingYears)
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { response in
-                print("서버 응답: \(response)")
-                completionHandler(response.status)
-            }.store(in: &cancellables)
-    }
-    
-    //흡연 정보 확인
-    func read(completionHandler: @escaping (Bool)->Void) {
-        SmokingHabitService.shared.readSmokingHabit(userId: userId)
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { response in
-                print("서버 응답: \(response)")
-                completionHandler(response.status)
-            }.store(in: &cancellables)
-    }
-    
-    //흡연 정보 수정
-    func update(completionHandler: @escaping (Bool)->Void) {
-        SmokingHabitService.shared.updateSmokingHabit(habitId: habitId, userId: userId, dailyCigarettes: dailyCigarettes, cigarettePrice: cigarettePrice, firstCigarette: firstCigarette, smokingYears: smokingYears)
+        SmokingHabitService.shared.createSmokingHabit(onboardingToken: onboardingToken, userID: userId, dailyCigarettes: dailyCigarettes, cigarettePrice: cigarettePrice, firstCigarette: firstCigarette, smokingYears: smokingYears)
             .receive(on: DispatchQueue.main)
             .sink { completion in
                 switch completion {
