70e622244c5ccdec14cd06f7ba02d1b71d8670cb
Eujeen Han <eujeenhan@gmail.com>
2024-07-25 11:38:32 +0900

    Update ContentView.swift

--- TimerTest/TimerTest/ContentView.swift
+++ TimerTest/TimerTest/ContentView.swift
@@ -8,22 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-   
     @ObservedObject var myTimer = MyTimer()
-    
+    let range1 = Date()...Date().addingTimeInterval(30)
+    let range2 = Date()...Date().addingTimeInterval(60)
     var body: some View {
         VStack {
             Text("\(self.myTimer.value)")
                 .font(.largeTitle)
             
-          
-            ProgressView(value: Float(self.myTimer.value), total: 100.0).padding()
+            ProgressView(timerInterval: range1).padding()
             
-            ProgressView(value: Float(self.myTimer.value), total: 10.0).padding()
+            ProgressView(timerInterval: range2, countsDown: false).padding()
+            
+            ProgressView(timerInterval: range2, countsDown: false) {
+                Text(String(format:"%.1f%%", (Double(self.myTimer.value)/60.0*100)))
+            }.padding()
         }
-        
-        
     }
 }
 
