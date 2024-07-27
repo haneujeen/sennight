e4b776a5f20880866fcb0da92033f136738a127a
sykr1313 <167267895+sykr1313@users.noreply.github.com>
2024-07-27 21:21:19 +0900

    Model 코드 작성 및 수정
    AidProduct read 코드 작성
    Milestone 코드 작성

--- Sennight/Sennight/Models/AidProduct.swift
+++ Sennight/Sennight/Models/AidProduct.swift
@@ -4,6 +4,30 @@
 //
 //  Created by 한유진 on 6/27/24.
 //  Edited by 한유진 on 2024-07-19: Factor.swift 파일 이름 수정
+//  Edited by 김소연 on 2024-07-27: AidProduct 모델 정의
 //
 
 import Foundation
+
+//Read AidProduct 모델 구현
+struct AidProductResponse: Codable {
+    let status: Bool
+    let detail: String
+    let data: [AidProduct]
+}
+
+struct AidProduct: Codable, Identifiable {
+    let id: Int
+    let aidProductID: Int
+    let userID: Int
+    let startDate: String
+    let endDate: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case aidProductID = "aid_product_id"
+        case userID = "user_id"
+        case endDate = "end_date"
+        case startDate = "start_date"
+    }
+}
--- Sennight/Sennight/Models/Milestone.swift
+++ Sennight/Sennight/Models/Milestone.swift
@@ -3,6 +3,31 @@
 //  Sennight
 //
 //  Created by 한유진 on 6/27/24.
+//  Edited by 김소연 on 2024-07-25: Milestone 모델 정의
 //
 
 import Foundation
+
+struct MilestoneResponse: Codable {
+    let status: Bool
+    let detail: String
+    let data: [Milestone]
+}
+
+struct Milestone: Codable, Identifiable {
+    let id: Int
+    let quitAttemptID: Int
+    let userID: Int
+    let milestoneID: Int
+    let dateAchieved: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case quitAttemptID = "quit_attempt_id"
+        case userID = "user_id"
+        case milestoneID = "milestone_id"
+        case dateAchieved = "date_achieved"
+    }
+}
+
+
