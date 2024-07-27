34f23ebcf7edb9c26b8f0249ad907d83f9621766
sykr1313 <167267895+sykr1313@users.noreply.github.com>
2024-07-27 21:22:08 +0900

    Milestones read 기능 구현

--- Sennight/Sennight/Views/MilestonePosts/MyMilestonesView.swift
+++ Sennight/Sennight/Views/MilestonePosts/MyMilestonesView.swift
@@ -3,60 +3,39 @@
 //  Sennight
 //
 //  Created by 한유진 on 7/2/24.
+//  Edited by 김소연 on 2024-07-25: Milestone view 정의
 //
 
 import SwiftUI
+var milestoneMessages = [
+    "Congratulations on reaching 7 days of being smoke-free! Your determination inspires us. Keep it up!",
+    "Two weeks smoke-free! It’s going to get easier from here. Stay strong!",
+    "Congrats on hitting the 3-week mark without smoking! You are a superstar."
+]
 
 struct MyMilestonesView: View {
+    @EnvironmentObject var logIn: LoginViewModel
+    @StateObject var milestionesViewModel = MilestoneViewModel()
+    
     var body: some View {
         HStack {
             VStack {
                 HStack {
-                    VStack {
-                        Image(systemName: "medal.fill")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 80, height: 80)
-                            .foregroundColor(.yellow)
-                        
-                        Text("7 Day Fresh")
+                    // List 사용하기
+                    List(milestionesViewModel.data) { data in
+                        Text("마일스톤 아이디: \(data.milestoneID)")
+                        if data.milestoneID - 1 < milestoneMessages.count && data.milestoneID - 1 >= 0 {
+                            Text(milestoneMessages[data.milestoneID - 1])
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
                     }
-                    .padding()
-                    .frame(width: 120)
-                    .background(.white)
-                    .cornerRadius(15)
-                    
-                    VStack {
-                        Image(systemName: "medal.fill")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 80, height: 80)
-                            .foregroundColor(.cyan)
-                        
-                        Text("30 Day Hero")
-                    }
-                    .padding()
-                    .frame(width: 120)
-                    .background(.white)
-                    .cornerRadius(15)
-                    
-                    VStack {
-                        Image(systemName: "medal.fill")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 80, height: 80)
-                            .foregroundColor(.teal)
-                        
-                        Text("3 Month Fresh")
-                    }
-                    .padding()
-                    .frame(width: 120)
-                    .background(.white)
-                    .cornerRadius(15)
+                    .onAppear(perform: {
+                        milestionesViewModel.read()
+                    })
                 }
                 Spacer()
             }
-            
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(.systemGray6))
