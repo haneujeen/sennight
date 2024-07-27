0286657ed23eac798eb4fb280b0b64e13bd2ab94
Eujeen Han <113181428+haneujeen@users.noreply.github.com>
2024-07-27 14:26:20 +0900

    UserMotivation 모델 리팩터링

--- Sennight/Sennight/Models/UserMotivation.swift
+++ Sennight/Sennight/Models/UserMotivation.swift
@@ -4,64 +4,42 @@
 //
 //  Created by 한유진 on 7/27/24.
 //  Edited by 김소연 on 2024-07-19: Motivation 모델 정의
+//  Edited by 한유진 on 2024-07-27: Motivation 모델 리팩터링
+/// 1. 파일 이름 변경: Motivation.swift --> UserMotivation.swift
+/// 2. MotivationReadRequest와 MotivationDeleteRequest 모델 삭제
+/// 3. MotivationRequest와 MotivationUpdateRequest 모델 병합 (UserMotivationRequest)
 //
 
 import Foundation
 
-struct MotivationRequest: Codable {
-    let userId: Int
-    let motivationId: Int
-   
-    enum CodingKeys: String, CodingKey {
-        case userId = "user_id"
-        case motivationId = "motivation_id"
-    }
-}
-
-struct MotivationReadRequest: Codable {
-    let userId: Int
-   
-    enum CodingKeys: String, CodingKey {
-        case userId = "user_id"
-    }
-}
-
-struct MotivationUpdateRequest: Codable {
-    let userId: Int
-    let motivationId: Int
-    let userMotivationId: Int
-    
-    enum CodingKeys: String, CodingKey {
-        case userId = "user_id"
-        case motivationId = "motivation_id"
-        case userMotivationId = "user_motivation_id"
-    }
-}
-
-struct MotivationDeleteRequest: Codable {
-    let userMotivationId: Int
-    
-    enum CodingKeys: String, CodingKey {
-        case userMotivationId = "user_motivation_id"
-    }
-}
-
-struct MotivationResponse: Codable {
-    let status : Bool
-    let detail : String
-    let data : MotivationData
-}
-
-struct MotivationData: Codable {
-    let id: Int
+struct UserMotivationRequest: Codable {
+    let userId: Int?
+    let motivationId: Int?
     let message: String?
+   
+    enum CodingKeys: String, CodingKey {
+        case userId = "user_id"
+        case motivationId = "motivation_id"
+        case message
+    }
+}
+
+struct UserMotivation: Codable {
+    let id: Int
     let userId: Int
     let motivationId: Int
+    let message: String
     
     enum CodingKeys: String, CodingKey {
         case id
-        case message
         case userId = "user_id"
         case motivationId = "motivation_id"
+        case message
     }
+}
+
+struct UserMotivationResponse: Codable {
+    let status : Bool
+    let detail : String
+    let data : UserMotivation?
 }
