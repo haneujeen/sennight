22758f21cd491e22e1b94df7f700f77b8caed13c
Eujeen Han <113181428+haneujeen@users.noreply.github.com>
2024-07-28 19:43:33 +0900

    Update MilestoneViewModel.swift

--- Sennight/Sennight/ViewModels/MilestoneViewModel.swift
+++ Sennight/Sennight/ViewModels/MilestoneViewModel.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import Combine
+
+class MilestoneViewModel: ObservableObject {
+    @Published var milestones: [UserMilestone] = []
+    @Published var maxMilestoneID = 0
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    func getAllMilestones() {
+        MilestoneService.shared.getAllMilestones()
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { milestones in
+                self.milestones = milestones
+            }
+            .store(in: &cancellables)
+    }
+    
+    func getMaxMilestoneID() {
+        MilestoneService.shared.getMaxMilestoneID()
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { maxMilestoneID in
+                self.maxMilestoneID = maxMilestoneID
+            }
+            .store(in: &cancellables)
+    }
+}
