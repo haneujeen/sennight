eb3fe4693713aa6d9d8935ada37f069de867e814
Eujeen Han <113181428+haneujeen@users.noreply.github.com>
2024-07-28 19:44:05 +0900

    Update MyMilestonesView.swift

--- Sennight/Sennight/Views/MilestonePosts/MyMilestonesView.swift
+++ Sennight/Sennight/Views/MilestonePosts/MyMilestonesView.swift
@@ -8,58 +8,21 @@
 import SwiftUI
 
 struct MyMilestonesView: View {
+    @State private var selectedOption = 0
+    
     var body: some View {
-        HStack {
-            VStack {
-                HStack {
-                    VStack {
-                        Image(systemName: "medal.fill")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 80, height: 80)
-                            .foregroundColor(.yellow)
-                        
-                        Text("7 Day Fresh")
-                    }
-                    .padding()
-                    .frame(width: 120)
-                    .background(.white)
-                    .cornerRadius(15)
-                    
-                    VStack {
-                        Image(systemName: "medal.fill")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 80, height: 80)
-                            .foregroundColor(.cyan)
-                        
-                        Text("30 Day Hero")
-                    }
-                    .padding()
-                    .frame(width: 120)
-                    .background(.white)
-                    .cornerRadius(15)
-                    
-                    VStack {
-                        Image(systemName: "medal.fill")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 80, height: 80)
-                            .foregroundColor(.teal)
-                        
-                        Text("3 Month Fresh")
-                    }
-                    .padding()
-                    .frame(width: 120)
-                    .background(.white)
-                    .cornerRadius(15)
-                }
-                Spacer()
+        VStack {
+            Picker("Filter", selection: $selectedOption) {
+                Text("All").tag(0)
+                Text("My Quit History").tag(1)
             }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
             
+            if selectedOption == 0 {
+            } else {
+            }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(.systemGray6))
     }
 }
 
