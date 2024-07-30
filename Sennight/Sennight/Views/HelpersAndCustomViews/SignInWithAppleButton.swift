25761493ec02601794d5f3ba4f0337323c2dbcc4
Eujeen Han <113181428+haneujeen@users.noreply.github.com>
2024-07-30 21:49:40 +0900

    Create SignInWithAppleButton.swift

---
+++ Sennight/Sennight/Views/HelpersAndCustomViews/SignInWithAppleButton.swift
@@ -0,0 +1,12 @@
+import Foundation
+import SwiftUI
+import AuthenticationServices
+
+struct SignInWithAppleButton: UIViewRepresentable {
+    func makeUIView(context: Context) -> ASAuthorizationAppleIDButton {
+        return ASAuthorizationAppleIDButton()
+    }
+    
+    func updateUIView(_ uiView: ASAuthorizationAppleIDButton, context: Context) {
+    }
+}
