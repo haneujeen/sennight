c68b47560e358fc915d77135947e32ba011976a2
Eujeen Han <113181428+haneujeen@users.noreply.github.com>
2024-08-02 12:22:30 +0900

    DashboardView 배경 색 추가

--- Sennight/Sennight/Views/Dashboard/DashboardView.swift
+++ Sennight/Sennight/Views/Dashboard/DashboardView.swift
@@ -13,32 +13,40 @@ struct DashboardView: View {
     
     var body: some View {
         NavigationStack {
-            List {
-                ElapsedTimeTextHeaderView()
-                    .frame(height: 60)
-                    .environmentObject(quitAttemptViewModel)
-                QuitAttemptCardView()
-                    .frame(height: 300)
-                    .environmentObject(quitAttemptViewModel)
-                MotivationCardView()
-                    .frame(height: 40)
-                    .environmentObject(quitAttemptViewModel)
-                MoneySavedCardView()
-                    .frame(height: 40)
-                    .environmentObject(quitAttemptViewModel)
-                AidProductsCardView()
-                SymptomsCardView()
-                ActivitiesCardView()
-            }
-            .onAppear {
-                quitAttemptViewModel.getActiveQuitAttempt()
-            }
-            .onChange(of: scenePhase) { oldPhase, newPhase in
-                if newPhase == .active {
-                    print("App became active")
+            ZStack {
+                Theme.buttercup.mainColor
+                    .ignoresSafeArea()
+                
+                List {
+                    ElapsedTimeTextHeaderView()
+                        .frame(height: 40)
+                        .listRowBackground(Theme.buttercup.mainColor)
+                        .environmentObject(quitAttemptViewModel)
+                    QuitAttemptCardView()
+                        .frame(height: 300)
+                        .environmentObject(quitAttemptViewModel)
+                    MotivationCardView()
+                        .frame(height: 40)
+                        .environmentObject(quitAttemptViewModel)
+                    MoneySavedCardView()
+                        .frame(height: 40)
+                        .environmentObject(quitAttemptViewModel)
+                    AidProductsCardView()
+                    SymptomsCardView()
+                    ActivitiesCardView()
+                }
+                .padding(.top, -20)
+                .scrollContentBackground(.hidden)
+                .onAppear {
                     quitAttemptViewModel.getActiveQuitAttempt()
-                } else {
-                    print("Scene phase changed to: \(newPhase)")
+                }
+                .onChange(of: scenePhase) { oldPhase, newPhase in
+                    if newPhase == .active {
+                        print("App became active")
+                        quitAttemptViewModel.getActiveQuitAttempt()
+                    } else {
+                        print("Scene phase changed to: \(newPhase)")
+                    }
                 }
             }
         }
